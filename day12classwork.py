597887ab6c80087222d31283f6d194aa2f97eb66
DavidBeria <143294896+DavidBeria@users.noreply.github.com>
2023-11-02 20:31:15 +0400

    Add files via upload

---
+++ day12classwork.py
@@ -0,0 +1,19 @@
+name = "დავით ბერია"
+
+for x in name:
+    if x == 'ა':
+        break
+print(x)
+
+
+#print(13%4)
+#print(17%5)
+# print(22%8)
+
+
+# x = [42, 8, 7, 1, 0, 124, 8897, 555, 3, 67, 99]
+
+# for num in x:
+#     if num == num:
+#         num + num
+# print(num)
---
+++ day12homework.py
@@ -0,0 +1,6 @@
+x = [42, 8, 7, 1, 0, 124, 8897, 555, 3, 67, 99]
+sum_of_numbers = 0
+
+for numbers in x:
+    sum_of_numbers += numbers
+print(sum_of_numbers)
